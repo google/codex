4af1df8e4cebe670058eb9c6bbe603e91a526227
Sung Jin Hwang <sjhwang@google.com>
2023-07-26 15:18:16 -0700

    Added placeholders for package build and test scripts.
    PiperOrigin-RevId: 551330023
    Change-Id: I7b62f825d489889e8112a62f9aab27f828316820

---
+++ tools/build_pip_package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex  # Fail if any command fails, echo commands.
+
+echo "PYTHON_BIN=${1}"
+
---
+++ tools/test_pip_package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex  # Fail if any command fails, echo commands.
+
+echo "PYTHON_BIN=${1}"
+echo "WHEEL_PATH=${2}"
+
