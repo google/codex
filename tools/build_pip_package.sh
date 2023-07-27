5570c0c7f81264ba91d88b7f71c94d44d8194614
Sung Jin Hwang <sjhwang@google.com>
2023-07-26 21:30:01 -0700

    Added commands to the build_pip_package script.
    PiperOrigin-RevId: 551403857
    Change-Id: Ida665e63ad55906a413040de7b39fa342f0d9b35

--- tools/build_pip_package.sh
+++ tools/build_pip_package.sh
@@ -2,5 +2,29 @@
 
 set -ex  # Fail if any command fails, echo commands.
 
-echo "PYTHON_BIN=${1}"
+PYTHON_BIN="${1}"
+
+# Creates clean virtual environment for build.
+"${PYTHON_BIN}" -m venv "${TEMP}/codex-build-venv"
+source "${TEMP}/codex-build-venv/bin/activate"
+python --version
+
+# Run all tests registered to bazel.
+# Currently there is no test configured in bazel. The following line
+# ends in error status if no test is found.
+# bazel test -c opt //... --test_output=all
+
+# Fetch the C++ library file.
+bazel build -c opt //codex/cc:range_ans_pybind.so
+cp $(bazel info -c opt bazel-genfiles)/codex/cc/range_ans_pybind.so \
+  $(bazel info -c opt workspace)/codex/cc/
+chmod =rw,+X $(bazel info -c opt workspace)/codex/cc/range_ans_pybind.so
+
+# Clean up bazel files.
+bazel clean
+
+python -m pip install -U flit pip setuptools wheel
+flit build
+
+deactivate
 
--- tools/test_pip_package.sh
+++ tools/test_pip_package.sh
@@ -2,6 +2,26 @@
 
 set -ex  # Fail if any command fails, echo commands.
 
-echo "PYTHON_BIN=${1}"
-echo "WHEEL_PATH=${2}"
+PYTHON_BIN="${1}"
+WHEEL_PATH="${2}"
+
+# Creates clean virtual environment for test.
+"${PYTHON_BIN}" -m venv "${TEMP}/codex-test-venv"
+source "${TEMP}/codex-test-venv/bin/activate"
+python --version
+
+# Installs built pip package.
+python -m pip install -U pip
+python -m pip install -U "${WHEEL_PATH}"
+python -m pip show -f jax-codex
+
+# Runs unit tests.
+python -m pip install -U pytest chex tensorflow-probability
+python -m pip list -v
+
+pushd "${TEMP}"
+python -m codex.all_tests
+popd
+
+deactivate
 
